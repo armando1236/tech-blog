3cef2645d235eec2734696ecf5d2c127fe45efde
amjr86-DBxjRd <amjr86@outlook.com>
2022-07-19 00:35:24 -0700

    working on postroutes.js

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const { response } = require('express');
+const res = require('express/lib/response');
 const sequelize = require('sequelize')
 const { User, Post } = require('../../models');
+const { getAttributes } = require('../../models/User');
 const withAuth = require('../../utils/auth');
 
 // /api/theater/13
@@ -14,63 +16,66 @@ router.get('/:id', async (req, res) => {
             },
             plain: true,
             })
-            console.log('Post Starts Here')
-            res.json(postRender)
-            console.log('--------------------------------------')
-            console.log(req.params.id)}
-        // const ratingAvgs = await Review.findAll({
-        //     where: {
-        //         theater_id: req.params.id,
-        //     },
-        //     plain: true,
-        //     attributes: [
-        //         [sequelize.literal(
-        //             '(SELECT AVG(seatingrating) FROM review)'
-        //         ),
-        //             'avgSeatingRating'
-        //         ],
-        //         [sequelize.literal(
-        //             '(SELECT AVG(concessionsrating) FROM review)'
-        //         ),
-        //             'avgConcessionsRating'
-        //         ],
-        //         [sequelize.literal(
-        //             '(SELECT AVG(audiorating) FROM review)'
-        //         ),
-        //             'avgAudioRating'
-        //         ],
-        //         [sequelize.literal(
-        //             '(SELECT AVG(videorating) FROM review)'
-        //         ),
-        //             'avgVideoRating'
-        //         ],
-        //         [sequelize.literal(
-        //             '(SELECT AVG(parkingrating) FROM review)'
-        //         ),
-        //             'avgParkingRating'
-        //         ],
-        //         [sequelize.literal(
-        //             '(SELECT AVG(servicerating) FROM review)'
-        //         ),
-        //             'avgServiceRating'
-        //         ],
-        //         [sequelize.literal(
-        //             '(SELECT AVG(crowdrating) FROM review)'
-        //         ),
-        //             'avgCrowdRating'
-        //         ],
-        //         [sequelize.literal(
-        //             '(SELECT AVG(crowdrating + servicerating + parkingrating + videorating + audiorating + concessionsrating + seatingrating)/7 FROM review)'
-        //         ),
-        //             'masterRating'
-        //         ],
-//             ]
-//         })
-//     } catch (err) {
-//         res.status(400).json(err);
-//     }
-// })
 
+            // console.log('Post Starts Here')
+            // res.json(postRender)
+            // console.log('--------------------------------------')
+            // console.log(req.params.id)}
+            include: [
+                {
+                    model: User,
+                    attributes: ['username']
+                },
+                {
+                    model: Comment,
+                    attributes: ['id', 'content',  'post_id', 'user_id'],
+                    include: {
+                        model: User,
+                        attributes: ['name']
+                    }
+                }
+            ]
+        });
+        .then(dbPostData => res.json(dbPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    });
+        router.post('/:id', withAuth, async (req, res)=>{
+            try{
+                console.log(req.body);
+                const newPost = await Post.create({
+                    ...req.body,
+                    title: req.body.title,
+                    content: req.body.content,
+                    user_id: req.session.user_id,
+                });
 
+                res.status(200).json(newReview);
+            } catch (err) {
+                console.log(err);
+                res.status(400).json(err);
+            }
+        });
 
+        router.delete('/:id', withAuth, async (req, res) => {
+            try{
+                const postData = await Post.destroy({
+                    where: {
+                        id: req.params.id,
+                        user_id: req.session.user_id,
+                    }
+                });
+                if (!postData) {
+                    res.status(400).json({ message: 'This post cannot be deleted'});
+                    return;
+                }
+                res.status(200).json(postData);
+            } catch (err) {
+                res.status(500).json(err);
+            }
+            
+        });
+        
 module.exports = router;
